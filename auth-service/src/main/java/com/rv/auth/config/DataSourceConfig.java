d7f5c762b0aa5b12f62ca535ccf3d2158d802757
yj1048883467@126.com <19941130>
2018-10-31 17:27:37 +0800

    添加了bus刷新配置的功能以及修复了一些bug

--- auth-service/src/main/java/com/rv/auth/config/DataSourceConfig.java
+++ auth-service/src/main/java/com/rv/auth/config/DataSourceConfig.java
@@ -2,6 +2,7 @@ package com.rv.auth.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
     @Bean
+    @RefreshScope
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource getMyDataSource() {
         return DataSourceBuilder.create().build();
